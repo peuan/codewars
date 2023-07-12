5ec17c25ddb4f751e0c831deb9b0a68dc5fc1d74
pasupol.bunsaen <niawkung@gmail.com>
2023-07-12 23:00:15 +0700

    update folder structure

--- divide and conquer/niaw_solution.js
+++
@@ -1,7 +0,0 @@
-function divCon(x) {
-  const n = x
-    .filter((v) => typeof v === "number")
-    .reduce((p, c) => (p += c), 0);
-  const s = x.filter((v) => typeof v === "string").reduce((p, c) => +p + +c, 0);
-  return n - s;
-}
--- mumbling/niaw_solution.js
+++
@@ -1,6 +0,0 @@
-function accum(s) {
-    return s.toLowerCase().split``
-      .map((v, i) => v.toUpperCase()+ v.repeat(i))
-      .join`-`
-}
-
