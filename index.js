d8bff2980bea9128e3356237a137e52d18c1659c
chokwatcharathep <chok.watcharathep@maqe.com>
2023-07-19 21:49:30 +0700

    fix(index): remove index

--- index.js
+++
@@ -1,5 +0,0 @@
-function printerError(s = '') {
-    return `${[...s].filter(c => c.charCodeAt() > 109).length}/${s.length}`
-}
-
-console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
