695f8db3fe31b5cedf073493464c72ceacfb376c
chokwatcharathep <chok.watcharathep@maqe.com>
2023-08-11 21:58:10 +0700

    task(level-7): my solution eiei

---
+++ 7 kyu/isTriangle/chok_solution.js
@@ -0,0 +1,34 @@
+function isTriangle(a, b, c) {
+    if (Math.min( a, b, c) < 1) {
+        return false
+    }
+
+    if (a + b > c && a + c > b && b + c > a) {
+        return true
+    } 
+
+    return (b === c) && b > a 
+}
+
+console.log(isTriangle(6,3,10), false);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+function randomSub() {
+    // Make coding great again!
+    let a = 'abcdefghijklmnopqrstuvwxyz';
+  return [...a].reduce((previous, current)  => {
+    const index = getRandomInt(a.length)
+
+    console.log(a)
+    previous[current] = a[index]
+    a = a.replace(a[index], '')
+    
+    return previous
+  }, {})
+}
+
+console.log(randomSub())
