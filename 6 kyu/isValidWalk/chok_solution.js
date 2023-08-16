d37bd41f1b261b4f0c3ee4aa18980c5eb987cdf2
chokwatcharathep <chok.watcharathep@maqe.com>
2023-08-16 22:50:30 +0700

    task(level-6): my solution eiei

--- 6 kyu/isValidWalk/chok_solution.js
+++ 6 kyu/isValidWalk/chok_solution.js
@@ -14,12 +14,12 @@ console.log(
   isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]),
   "should return true"
 );
-console.log(
-  isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"]),
-  "should return false"
-);
-console.log(isValidWalk(["w"]), "should return false");
-console.log(
-  isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]),
-  "should return false"
-);
+// console.log(
+//   isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"]),
+//   "should return false"
+// );
+// console.log(isValidWalk(["w"]), "should return false");
+// console.log(
+//   isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]),
+//   "should return false"
+// );
