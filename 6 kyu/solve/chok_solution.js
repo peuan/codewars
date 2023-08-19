2c3fcb1912c03a4a98c7d0389c85704b2c936968
chokwatcharathep <chok.watcharathep@maqe.com>
2023-08-19 12:37:06 +0700

    task(level-6): my solution eiei

---
+++ 6 kyu/solve/chok_solution.js
@@ -0,0 +1,21 @@
+function solve(s = "") {
+  const numberToConvert = 96;
+  return Math.max(...s
+    .split(/[a,e,i,o,u]/g)
+    .map((char) => {
+        return [...char].reduce((current, previous) => {
+            current += previous.charCodeAt() - numberToConvert
+
+            return current
+        }, 0)
+    }))
+}
+
+console.log(solve("zodiac"), 26);
+console.log(solve("chruschtschov"), 80);
+console.log(solve("khrushchev"), 38);
+console.log(solve("strength"), 57);
+console.log(solve("catchphrase"), 73);
+console.log(solve("twelfthstreet"), 103);
+console.log(solve("mischtschenkoana"), 80);
+console.log(solve("az"), 26);
