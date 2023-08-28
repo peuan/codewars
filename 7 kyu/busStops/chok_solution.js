6b16858c18f0e3aea70d98cfd9e0ebc82198638e
chokwatcharathep <chok.watcharathep@maqe.com>
2023-08-28 21:46:33 +0700

    task(level-7): my solution eiei

---
+++ 7 kyu/busStops/chok_solution.js
@@ -0,0 +1,37 @@
+var number = function (busStops) {
+  return busStops.reduce((previous, [getIn, getOff]) => {
+    return previous + getIn - getOff
+  }, 0)
+};
+
+console.log(
+  number([
+    [10, 0],
+    [3, 5],
+    [5, 8],
+  ]),
+  5
+);
+console.log(
+  number([
+    [3, 0],
+    [9, 1],
+    [4, 10],
+    [12, 2],
+    [6, 1],
+    [7, 10],
+  ]),
+  17
+);
+console.log(
+  number([
+    [3, 0],
+    [9, 1],
+    [4, 8],
+    [12, 2],
+    [6, 1],
+    [7, 8],
+  ]),
+  21
+);
+console.log(number([[0, 0]]), 0);
