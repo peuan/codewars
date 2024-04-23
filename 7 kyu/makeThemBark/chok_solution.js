f4332414f0f9d110dddc71e87749323555e853ca
chokwatcharathep <chok.watcharathep@maqe.com>
2024-04-23 21:10:44 +0700

    task(level-7): my function as class solution

---
+++ 7 kyu/makeThemBark/chok_solution.js
@@ -0,0 +1,10 @@
+function Dog(name, breed, sex, age) {
+  this.name = name;
+  this.breed = breed;
+  this.sex = sex;
+  this.age = age;
+
+  this.bark = function () {
+    return "Woof!";
+  };
+}
--- 7 kyu/validateWord/chok_solution.js
+++ 7 kyu/validateWord/chok_solution.js
@@ -9,7 +9,7 @@ function validateWord(s = "") {
     }, {});
 
   const counts = Object.values(counter);
-  const max = Math.max(...Object.values(counter));
+  const max = Math.max(...counts);
 
   return counts.every((count) => count === max);
 }
@@ -17,6 +17,6 @@ function validateWord(s = "") {
 console.log(validateWord("abcabc"), true, 'The word was: "abcabc" \n');
 console.log(validateWord("Abcabc"), true, 'The word was: "Abcabc" \n');
 // console.log(validateWord("abc123"),true, "The word was: \"abc123\" \n");
-// console.log(validateWord("abcabcd"),false, "The word was: \"abcabcd\" \n");
+console.log(validateWord("abcabcd"),false, "The word was: \"abcabcd\" \n");
 // console.log(validateWord("abc!abc!"),true, "The word was: \"abc!abc!\" \n");
 // console.log(validateWord("abc:abc"),false, "The word was: \"abc:abc\" \n");
